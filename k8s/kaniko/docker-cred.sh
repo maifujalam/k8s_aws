1470ec20ee9c87ac321c8cc270f969fc50bc7190
ALAM <sk.maifujalam@gmail.com>
2025-02-14 21:14:41 +0530

    K8S

--- k8s/kaniko/docker-cred.sh
+++ k8s/kaniko/docker-cred.sh
@@ -1,2 +1,3 @@
-kubectl -n jenkins create secret docker-registry regcred --docker-server=skmaifujalam/evicted-pod-cleaner \
+kubectl -n jenkins delete secret regcred
+kubectl -n jenkins create secret docker-registry regcred --docker-server=https://index.docker.io/v1/  \
 --docker-username=skmaifujalam --docker-password=Newman123@ --docker-email=sk.maifujalam@gmail.com
