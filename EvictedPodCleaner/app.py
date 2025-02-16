678d79dfb9391b4045df948fe3f5ee998ad9336b
ALAM <sk.maifujalam@gmail.com>
2025-02-16 19:52:38 +0530

    K8S

--- EvictedPodCleaner/app.py
+++ EvictedPodCleaner/app.py
@@ -1,20 +1,25 @@
 #!/bin/python3
+from time import sleep
+
 import kubernetes
 
 if __name__ == '__main__':
     kubernetes.config.load_kube_config()
     v1=kubernetes.client.CoreV1Api()
-    pods=v1.list_pod_for_all_namespaces(watch=False)
-    count=0
-    for pod in pods.items:
-        if pod.status.phase != 'Running':
-            print(pod.metadata.name+ " | "+pod.metadata.namespace+" | "+pod.status.phase)
-        try:
-            if pod.status.reason:
-                if pod.status.reason == "Evicted":
-                    print("Deleting Evicted Pod "+pod.metadata.name)
-                    v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
-                    print("Deleted Evicted Pod "+pod.metadata.name)
+    while True:
+        print("Sleeping 5 sec...")
+        sleep(5)
+        pods=v1.list_pod_for_all_namespaces(watch=False)
+        count=0
+        for pod in pods.items:
+            if pod.status.phase != 'Running':
+                print(pod.metadata.name+ " | "+pod.metadata.namespace+" | "+pod.status.phase)
+            try:
+                if pod.status.reason:
+                    if pod.status.reason == "Evicted":
+                        print("Deleting Evicted Pod "+pod.metadata.name)
+                        v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+                        print("Deleted Evicted Pod "+pod.metadata.name)
 
-        except:
-            print("Failed to get pod status")
+            except:
+                print("Failed to get pod status")
--- k8s/kaniko/docker-cred.sh
+++
@@ -1,3 +0,0 @@
-kubectl -n jenkins delete secret regcred
-kubectl -n jenkins create secret docker-registry regcred --docker-server=https://index.docker.io/v1/  \
---docker-username=skmaifujalam --docker-password=Newman123@ --docker-email=sk.maifujalam@gmail.com
